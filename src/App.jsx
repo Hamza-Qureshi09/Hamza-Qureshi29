fa10161d0568aef8d85f4170a6e7cdf1f9008d2e
ahmad <ahmad03226110228@gmail.com>
2022-12-26 16:06:08 +0500

    initial release

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+    
+      <h1>Hamza Qureshi Portfolio</h1>
+      
+      <p className="read-the-docs">
+        uploading Portfolio first time in vercel.
+      </p>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
